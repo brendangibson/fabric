a7e527f67351bec1ad14d4fdf35d8ac3b0fff77a
Brendan Gibson <brendan.gibson@drip.com>
2018-11-24 08:06:30 -0700

    Scroll to top on page transitions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import Header from './Components/Header'
+import Header from "./Components/Header";
+import ScrollToTop from "./Components/ScrollToTop";
 
 const wrapperStyle = {
-    padding: "0 5vw 5vw 5vw"
-}
+  padding: "0 5vw 5vw 5vw"
+};
 
-const App = (props) => (
+const App = props => (
+  <ScrollToTop>
     <div style={wrapperStyle} className="fabricApp">
-        <Header />
-        {props.children}
+      <Header />
+      {props.children}
     </div>
-)
+  </ScrollToTop>
+);
 
-export default App
+export default App;
