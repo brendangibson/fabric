c2335978142becdf099d2903deeaf91b54c6ccfe
Brendan Gibson <github@petskink.net>
2020-08-19 09:01:32 -0600

    Network requests for homepage; new images

--- src/Components/StylesColours.js
+++ src/Components/StylesColours.js
@@ -36,7 +36,7 @@ const renderStyleColour = (stylecolour) => {
 };
 
 const StylesColours = () => (
-  <Query query={QueryGetStylesColours}>
+  <Query query={QueryGetStylesColours} fetchPolicy="network-only">
     {({ loading, error, data }) => {
       if (loading) return <Loading />;
       if (error) return `Error! ${error.message}`;
